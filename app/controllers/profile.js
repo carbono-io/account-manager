a2de9e0e449bab4a8dd6b97dd1cdb4494be3c08b
Aline Takechi <akt@cesar.org.br>
2015-09-09 14:41:38 -0300

    [JRD-344] refactoring

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -1,74 +1,15 @@
 'use strict';
-var CJM = require('carbono-json-messages');
 
 /**
- * Checks if the received message is compatible with the API's defined
- * structure.
+ * Controller for handling profile related requests.
  *
- * @param {Object} message - Express object representing a Request
- * @return {boolean} True if the structure is correct. False otherwise.
- *
- * @function
+ * @author Carbono Team
+ * @module profile
  */
-var checkMessageStructure = function (message) {
-    return message &&
-        message.body &&
-        message.body.id &&
-        message.body.apiVersion === '1.0' &&
-        message.body.data &&
-        message.body.data.id &&
-        message.body.data.items &&
-        message.body.data.items[0];
-};
-
-/**
- * Checks if the data has all the required properties.
- *
- * @param {Object} data - Object which will be checked.
- * @param {string[]} required - Required properties
- * @return {string[]} All missing properties.
- */
-var checkRequiredData = function (data, required) {
-    var missing = [];
-    required.forEach(function (prop) {
-        if (!data.hasOwnProperty(prop)) {
-            missing.push(prop);
-        }
-    });
-    return missing;
-};
-
-/**
- * Creates a response message, according to API's defined structure.
- *
- * @param {Object} res - Express object representing Response.
- * @param {number} htmlCode - HTML Code for this Response.
- * @param {string=|Object=} message - If htmlCode is 200, this param will be the
- * detail to be appended inside 'items[]' array. If it's another htmlCode, it
- * will use the error template, and this param will be included at 'message'
- * attribute.
- *
- * @function
- */
-var createResponse = function (res, htmlCode, message) {
-    res.status(htmlCode);
-
-    if (message) {
-        var cjm = new CJM({apiVersion: '1.0'});
-        if (htmlCode === 200) {
-            cjm.setData(message);
-        } else {
-            cjm.setError({
-                code: htmlCode,
-                message: message,
-            });
-        }
-        res.json(cjm);
-    }
-    res.end();
-};
-
 module.exports = function (app, models) {
+
+    var RequestHelper = require('../lib/requestHelper.js');
+    var reqHelper = new RequestHelper();
 
     /**
      * Creates a new profile.
@@ -83,29 +24,34 @@ module.exports = function (app, models) {
      -H "Content-Type: application/json"
      */
     this.create = function (req, res) {
-        if (checkMessageStructure(req)) {
+        if (reqHelper.checkMessageStructure(req)) {
             var userData = req.body.data.items[0];
             var missingProperties =
-                checkRequiredData(
+                reqHelper.checkRequiredData(
                     userData, ['code', 'name', 'email', 'password']);
 
             if (missingProperties.length) {
                 missingProperties.forEach(function (prop) {
-                    createResponse(res, 400,
+                    reqHelper.createResponse(res, 400,
                         'Malformed request: ' + prop + ' is required.');
                 });
             } else {
                 // TODO: código do vitor aqui
 
-                createResponse(res, 200);
+                reqHelper.createResponse(res, 200);
             }
         } else {
-            createResponse(res, 400, 'Malformed request');
+            reqHelper.createResponse(res, 400, 'Malformed request');
         }
     };
 
     /**
      * Returns an profile user based on a ID.
+     *
+     * @param {Object} req - Request object
+     * @param {Object} res - Response object
+     *
+     * curl example: curl localhost:7888/profiles/123456 --verbose
      */
     this.retrieve = function (req, res) {
         if (req.params && req.params.code) {
@@ -113,9 +59,10 @@ module.exports = function (app, models) {
 
             // TODO: código do vitor aqui
             // createResponse(res, 404, 'profile not found');
-            createResponse(res, 200);
+            reqHelper.createResponse(res, 200);
         } else {
-            createResponse(res, 400, 'Malformed request: id is required.');
+            reqHelper.createResponse(res, 400,
+                'Malformed request: id is required.');
         }
     };
 
