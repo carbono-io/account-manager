8c1bf93c1dc15accebb4551a169912012501cbd8
Paul <deprazz@gmail.com>
2015-10-01 14:32:14 -0300

    [JRD-525] Changed etcd manager

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
+
 var bodyParser = require('body-parser');
 var consign   = require('consign');
 var express   = require('express');
 var config    = require('config');
-var EtcdManager = require('./app/lib/etcd-manager.js');
 require('colors');
+
 var app       = express();
 var baseApp   = express();
 
@@ -12,7 +13,7 @@ app.set('models', require('./app/models'));
 app.use(bodyParser.json());
 app.use('/account-manager', baseApp);
 
-consign({cwd: 'app'})
+consign({cwd: process.cwd() + '/app'})
     .include('lib')
     .include('controllers')
     .include('routes')
@@ -22,8 +23,7 @@ var server = app.listen(config.get('port'), function () {
     var port = config.get('port');
     var host = config.get('host');
     console.log('Imperial listening at http://%s:%s', host, port);
-    var etcdManager = new EtcdManager();
-    etcdManager.init();
+    require('carbono-service-manager');
 });
 
 module.exports = server;
